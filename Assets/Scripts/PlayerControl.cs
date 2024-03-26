b879b259b3e14449dba086f29fd73fefa25b33d5
CatBio908 <alexbsledge@gmail.com>
2024-03-26 18:52:52 -0500

    Update PlayerControl.cs

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,24 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class PlayerControl : MonoBehaviour
 {
     public int player = 1;
     public float MoveSpeed = 5f;
-    
-    
-    
+    public float Damage = 5f;
+    public float Health = 10f;
+
+    Rigidbody = rb
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nkjij
+
     }
 }
