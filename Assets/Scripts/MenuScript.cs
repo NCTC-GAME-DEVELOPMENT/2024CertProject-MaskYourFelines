76809b354f89f6d1189b06cd6746fe03c2451107
CatBio908 <alexbsledge@gmail.com>
2024-05-07 17:50:26 -0500

    Menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -7,17 +7,11 @@ public class MenuScript : MonoBehaviour
 {
     public string sceneName;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void QuitGame()
+   {
+        Application.Quit();
+        Debug.Log("Quit!");
+   }
 
     public void changeScene()
     {
