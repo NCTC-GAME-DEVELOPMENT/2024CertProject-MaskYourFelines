b0e9422a044ac8ff865036afa4ed52b77e0c622c
Veilstrom <tjporygon2@gmail.com>
2024-05-08 17:13:11 -0500

    misc

--- Assets/Sprite Assets/AnimScriptTests/PlayerManager_2.cs
+++ Assets/Sprite Assets/AnimScriptTests/PlayerManager_2.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.Intrinsics;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
